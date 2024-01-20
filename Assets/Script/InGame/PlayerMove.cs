1094d14168492f6d3ec9659f973318ea2cf5a423
kuromaaaaa <kuromaxa@gmail.com>
2024-01-20 12:47:28 +0900

    アイドル、バックの追加

--- Assets/Script/InGame/PlayerMove.cs
+++ Assets/Script/InGame/PlayerMove.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float _movePower = 5;
+    [SerializeField] float _backSpeed = 4;
     Rigidbody _rb;
     PlayerInput _input;
     PlayerData _pd;
     Animator _anim;
 
     bool _moveForward;
+    bool _moveBack;
     
     // Start is called before the first frame update
     void Start()
@@ -31,19 +33,24 @@ public class PlayerMove : MonoBehaviour
         {
             _rb.velocity = new Vector3(direction.x * _movePower, _rb.velocity.y, 0);
             _moveForward = true;
+            _moveBack = false;
         }
         else if(_input.TenKey == 4 && !_pd.Damaging)
         {
-            _rb.velocity = new Vector3(direction.x * _movePower * -1, _rb.velocity.y, 0);
+            _rb.velocity = new Vector3(direction.x * _backSpeed * -1, _rb.velocity.y, 0);
+            _moveBack = true;
+            _moveForward = false;
         }
         else if(!_pd.Damaging)
         {
             _rb.velocity = new Vector3(0, _rb.velocity.y, 0);
             _moveForward = false;
+            _moveBack = false;
         }
         if(_anim)
         {
             _anim.SetBool("MoveForward", _moveForward);
+            _anim.SetBool("MoveBack", _moveBack);
         }
     }
     void OnAnimatorMove()
